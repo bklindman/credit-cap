8833d583c02d912e9cf505c246c9e68996ba6a6f
bklindman <blake.lindman@gmail.com>
2019-02-10 15:08:39 -0800

    require mongoose var from mongoose.js into main app

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const config = require('./config/config');
-
+const {mongoose} = require('./db/mongoose');
 const port = process.env.PORT;
 
 var app = express();
