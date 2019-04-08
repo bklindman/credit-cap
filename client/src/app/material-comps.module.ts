ef0a3656f065078786a6d09f6ef08ad2900b1691
bklindman <blake.lindman@gmail.com>
2019-04-07 19:06:05 -0700

    import more material modules

--- client/src/app/material-comps.module.ts
+++ client/src/app/material-comps.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   imports: [
-    MatToolbarModule
+    MatToolbarModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatDividerModule,
+    MatButtonModule
   ],
   exports: [
-    MatToolbarModule
+    MatToolbarModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatDividerModule,
+    MatButtonModule
   ]
 })
 export class MaterialCompsModule { }
