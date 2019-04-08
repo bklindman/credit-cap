a4c31ff1ffedc0f0ba46592e4a5ae47a47effd3a
bklindman <blake.lindman@gmail.com>
2019-04-07 19:07:19 -0700

    add import forms module to app module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -7,14 +8,17 @@ import { MaterialCompsModule } from './material-comps.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { RoutingModule } from './routing.module';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     BrowserAnimationsModule,
     MaterialCompsModule,
     FlexLayoutModule,
