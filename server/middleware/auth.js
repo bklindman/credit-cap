13148e77458639b60de3acfa748dad29d9bb64d9
bklindman <blake.lindman@gmail.com>
2019-04-10 14:39:21 -0700

    add user auth middleware, access cookie via req.cookies from cookieParser

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,22 +1,18 @@
 const jwt = require('jsonwebtoken'); 
 const secret = process.env.SECRET;
 
-var authAdmin = (req, res, next) => {
+var auth = (req, res, next) => {
   var token = req.cookies['x-auth-token'];
   if(!token){
     return res.status(401).send({message: 'No token provided'});
   }
   jwt.verify(token, secret, function(err, decoded) {
-    if(err) {
-      return res.status(400).send({message: 'Failed to authenticate token.'});
-    } else if (decoded.role !== 'admin'){
-      return res.status(403).send();
-    }
+    if(err) return res.status(400).send({message: 'Failed to authenticate token.'});
     next();
   });
 };
 
 
 module.exports = {
-  authAdmin
+  auth
 }
