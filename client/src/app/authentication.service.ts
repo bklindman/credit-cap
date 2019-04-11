3aa0b4ce1e72147ab5e59cb17e989a695cdecb98
bklindman <blake.lindman@gmail.com>
2019-04-11 10:55:40 -0700

    add httpclient, login/signout functionality

--- client/src/app/authentication.service.ts
+++ client/src/app/authentication.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { UserLogin } from './interfaces/UserLogin';
+import { AuthResult } from './interfaces/AuthResult';
+import { UserSignup } from './interfaces/UserSignup';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+  authUrl = "/auth/users";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  signUp(user: UserSignup){
+    return this.http.post<AuthResult>(this.authUrl, user);
+  }
+
+  logIn(user: UserLogin){
+    return this.http.post<AuthResult>(this.authUrl+"/login", user);
+  }
+
+  logOut() {
+    return this.http.get(`${this.authUrl}/logout`);
+  }
+
+  isAuthenticated(): boolean {
+    let info = localStorage.getItem('user');
+    if(!info){
+      return false;
+    }
+    let user: AuthResult = JSON.parse(info);
+    let expired: boolean = (new Date(user.expiry)) < new Date();
+    if (expired) this.deleteLocalStorage();
+    return !expired;
+  }
+
+  public deleteLocalStorage(): void {
+    localStorage.removeItem('user');
+  }
 }
---
+++ client/src/app/interfaces/AuthResult.ts
@@ -0,0 +1,4 @@
+export interface AuthResult{
+  expiry: string;
+  auth: boolean;
+}
