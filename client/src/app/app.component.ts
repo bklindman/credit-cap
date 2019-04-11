1a613bb0fda3fb04b204af3c765e2c9dad2eaf6c
bklindman <blake.lindman@gmail.com>
2019-04-11 11:02:29 -0700

    add login button link to navbar

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from './authentication.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+
+  constructor(private authService: AuthenticationService){
+
+  }
+
+  onLogout(){
+    this.authService.logOut().subscribe(res => {
+      this.authService.deleteLocalStorage();
+    });
+  }
 }
