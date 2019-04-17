70a39d641842ad6d9d754609ed7075e205809959
bklindman <blake.lindman@gmail.com>
2019-04-16 17:41:49 -0700

    create Item schema, embed in User Schema

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -2,10 +2,19 @@ var mongoose = require('mongoose');
 const {Account} = require('./account');
 
 
-var UserSchema = new mongoose.Schema({
-  tokens: {
-    type: [String]
+var ItemSchema = new mongoose.Schema({
+  item_id: {
+    type: String,
+    required: true
   },
+  access_token: {
+    type: String,
+    required: true
+  }
+}, {_id: false});
+
+var UserSchema = new mongoose.Schema({
+  items: [ItemSchema],
   linked: {
     type: Boolean,
     default: false
