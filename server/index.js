87d5bf3906e119da7b160b10b41227b4a2bd88a7
bklindman <blake.lindman@gmail.com>
2019-05-04 12:24:14 -0700

    add user router to main router

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const path = require('path');
 
 var auth = require('./controllers/authController');
 var link_handler = require('./controllers/plaidController');
+var userRoute = require('./controllers/userController');
 
 const port = process.env.PORT;
 
@@ -21,6 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname,'public')));
 app.use('/auth', auth);
 app.use('/plaid', link_handler);
+app.use('/user', userRoute);
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
