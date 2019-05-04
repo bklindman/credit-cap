8ddb4dd2cf2f03996f533de18cfb7d2a8c6ded6e
bklindman <blake.lindman@gmail.com>
2019-05-04 12:40:49 -0700

    refactor code

--- server/controllers/plaidController.js
+++ server/controllers/plaidController.js
@@ -22,23 +22,45 @@ var client = new plaid.Client(
 
 
 router.post('/get_access_token', auth, (req, res) => {
-  var PUBLIC_TOKEN = req.body.public_token;
-  client.exchangePublicToken(PUBLIC_TOKEN, (err, tokRes) => {
-    if(err){
-      console.log('Could not exchange public token');
-      return res.status(400).send({error: err.message});
-    }
-    var ACCESS_TOKEN = tokRes.access_token;
-    var ITEM_ID = tokRes.item_id;
-    User.findById(req.userId).then((user) => {
-      user.items.push({item_id: ITEM_ID, access_token: ACCESS_TOKEN});
-      user.save((err) => {if(err)console.log(err)});
-    }).catch((e) =>{
-      console.log(e);
-      return res.status(400).send();
+  getTokenResponse(req.body.public_token).then((tokRes) => {
+    return storeBankInfo(req.userId, tokRes.item_id, tokRes.access_token, req.body.metadata).then(() => {
+      res.status(200).send();
     });
-    return res.status(200).send();
+  })
+  .catch((e) => {
+    return res.status(400).send(e);
   });
 });
 
 module.exports = router;
+
+
+function storeBankInfo(userId, item_id, access_token, metadata) {
+  return new Promise((resolve, reject) => {
+    User.findById(userId).then((user) => {
+      user.items.push(createItem(item_id, access_token, metadata));
+      user.linked = true;
+      user.save((err) => { if (err) reject(err)});
+      resolve();
+    }).catch((e) => { 
+      reject(e);
+    });
+  });
+}
+
+function createItem(item_id, access_token, metadata){
+  let item = {item_id, access_token, institution_name: metadata.institution.name}
+  item.accounts = metadata.accounts.filter((account) => {
+    ['depository', 'credit'].includes(account.type)
+  }).map(account => account.name);
+  return item;
+}
+
+function getTokenResponse(public_token) {
+  return new Promise((resolve, reject) => {
+    client.exchangePublicToken(public_token, (err, tokenResponse) => {
+      if (err) reject('Could not exchange public token');
+      resolve(tokenResponse);
+    });
+  });
+}
