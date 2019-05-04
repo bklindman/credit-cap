c98ae83c44005dd65a02f185a5a438c8cbc839d2
bklindman <blake.lindman@gmail.com>
2019-05-04 12:46:33 -0700

    add observable in authentication service, minor changes to methods

--- client/src/app/authentication.service.ts
+++ client/src/app/authentication.service.ts
@@ -3,13 +3,15 @@ import { HttpClient, HttpHeaders} from '@angular/common/http';
 import { UserLogin } from './interfaces/UserLogin';
 import { UserStore } from './interfaces/UserStore';
 import { UserSignup } from './interfaces/UserSignup';
+import { Subject, BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
   authUrl = "/auth/users";
-
+  private user: UserStore;
+  public userStoreChange: BehaviorSubject<any> = new BehaviorSubject<any>({});
   constructor(private http: HttpClient) { }
 
   signUp(user: UserSignup){
@@ -37,13 +39,24 @@ export class AuthenticationService {
 
   public deleteLocalStorage(): void {
     localStorage.removeItem('user');
+    this.userStoreChange.next(null);
   }
 
   public getUserStore(): UserStore{
     if(this.isAuthenticated()){
-      let info = localStorage.getItem('user');
-      let user: UserStore = JSON.parse(info);
-      return user;
+      return this.user;
     }
+  }
+
+  public setUserStore(res: UserStore): void{
+    localStorage.setItem("user", JSON.stringify(res));
+    this.user = res;
+    this.userStoreChange.next(this.user);
+  }
+
+  public updateUserStore(key, value){
+    this.user[key] = value;
+    localStorage.setItem('user', JSON.stringify(this.user));
+    this.userStoreChange.next(this.user);
   }
 }
