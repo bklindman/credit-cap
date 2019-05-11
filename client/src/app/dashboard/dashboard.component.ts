65c4e110374caf8f057c290ff9743ddef910664d
bklindman <blake.lindman@gmail.com>
2019-05-11 10:47:57 -0700

    display user data on dashboard

--- client/src/app/dashboard/dashboard.component.ts
+++ client/src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,13 @@
-import { Component, OnInit, ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, ChangeDetectionStrategy, AfterViewInit } from '@angular/core';
 import { AuthenticationService } from 'src/app/authentication.service';
 import { environment } from '../../environments/environment';
 import { PlaidLinkService } from '../services/plaid-link.service';
 import { UserStore } from '../interfaces/UserStore';
-import { Subject, Observable, BehaviorSubject } from 'rxjs';
+import { UserService } from '../services/user.service';
+import { UserBank } from '../interfaces/UserBank';
+import { MediaObserver, MediaChange } from '@angular/flex-layout';
+import { Subscription } from 'rxjs';
+import * as chartSettings from './piechart-properties';
 declare let Plaid: any;
 
 @Component({
@@ -14,12 +18,42 @@ declare let Plaid: any;
 export class DashboardComponent implements OnInit {
   public user: UserStore;
   private handler: any;
+  mediaQuery$: Subscription;
+  activeMediaQuery: string;
+  public userBanks: UserBank[] = [];
+  expenses: any[];
+  
+  showLegend: boolean = chartSettings.showLegend;
+  showLabels: boolean = chartSettings.showLabels;
+  legendPosition: String = chartSettings.legendPosition;
+  colorScheme = chartSettings.colorScheme;
 
-  constructor(private authService: AuthenticationService, private plaidService: PlaidLinkService) {}
+  constructor(private authService: AuthenticationService,
+     private plaidService: PlaidLinkService, 
+     private userService: UserService,
+     private mediaObserver: MediaObserver) {
+       this.subscribeToMedia();
+     }
+
+  private subscribeToMedia(): void {
+    this.mediaQuery$ = this.mediaObserver.media$.subscribe((change: MediaChange) => {
+      this.activeMediaQuery = change.mqAlias;
+      if(this.activeMediaQuery === 'xs' || this.activeMediaQuery === 'sm'){
+        this.showLabels = false;
+        this.legendPosition = 'below';
+      } else {
+        this.showLabels = true;
+        this.legendPosition = 'right';
+      }
+    });
+  }
 
   ngOnInit() {
     this.authService.userStoreChange.subscribe((user) => {
       this.user = user;
+      if(user && user.linked && !this.userBanks.length){
+        this.getAccounts();
+      }
     });
     let key = environment.key;
     this.handler = Plaid.create({
@@ -38,6 +72,7 @@ export class DashboardComponent implements OnInit {
   getPublicToken(token: String, metadata: String){
     this.plaidService.integrateLink(token, metadata).subscribe((res) => {
       this.authService.updateUserStore("linked", true);
+      this.getAccounts();
     });
   }
 
@@ -49,4 +84,16 @@ export class DashboardComponent implements OnInit {
     return this.authService.getUserStore().name;
   }
 
+  private getAccounts(): void {
+    this.userService.getAccounts().subscribe((accounts) => {
+      this.userBanks = accounts;
+      this.getPurchaseOverview();
+    });
+  }
+
+  private getPurchaseOverview(): void {
+    this.userService.getExpensesOverview().subscribe((categories) => {
+      this.expenses = categories;
+    });
+  }
 }
---
+++ client/src/app/dashboard/piechart-properties.ts
@@ -0,0 +1,19 @@
+export var showLegend: boolean = true;
+export var showLabels: boolean = true;
+export var legendPosition: String = 'right';
+export var colorScheme = {
+  domain:[
+  "#00e677",
+  "#5ee05a",
+  "#85d93f",
+  "#a3d221",
+  "#bdc900",
+  "#d5be00",
+  "#ebb300",
+  "#ffa600",
+  "#006fe6",
+  "#ab5ed9",
+  "#f34ab2",
+  "#ff517d",
+  "#ff7847"]
+}
