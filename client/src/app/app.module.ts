28b4c2f63bab2a9020e6b3c19745d8406d0b450a
bklindman <blake.lindman@gmail.com>
2019-05-11 10:45:32 -0700

    add ngx-charts module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,10 +8,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialCompsModule } from './material-comps.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { RoutingModule } from './routing.module';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { AuthenticationService } from './authentication.service';
 import { DashboardComponent } from './dashboard/dashboard.component';
+
 
 @NgModule({
   declarations: [
@@ -29,6 +31,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     RoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
+    NgxChartsModule
   ],
   providers: [AuthenticationService],
   bootstrap: [AppComponent]
