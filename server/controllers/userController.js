765cc652116e30ec86132c7a860713bf426e7b66
bklindman <blake.lindman@gmail.com>
2019-05-14 07:52:36 -0700

    refactor code

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { auth } = require('../middleware/auth');
 const { User } = require('../db/models/user');
+const { CreditCard } = require('../db/models/credit_card');
 var router = express.Router();
 const moment = require('moment');
 const { client } = require('../utils/plaid-client');
@@ -28,25 +29,41 @@ function mapBanksToAccounts(user) {
 }
 
 router.get('/expenses/categories', auth, (req, res) => {
-  User.findById(req.userId).then((user) => {
+  getPurchases(req.userId, getCategoryProportions).then((proportions)=> {
+    return res.send(proportions);
+  });
+});
+
+router.get('/recommend', auth, (req, res) => {
+  getPurchases(req.userId, getCards).then((cards) => {
+    return res.send(cards);
+  });
+});
+
+function getPurchases(userId, callback){
+  let nonPurchaseCategories = ['Bank Fees', 'Cash Advance' , 'Interest',  'Payment', 'Tax' ,'Transfer'];
+  return User.findById(userId).then((user) => {
     var transactions = [];
     let promises = [];
     for (let item of user.items) {
       promises.push(client.getAllTransactions(item.access_token, '1970-01-01', moment().format('YYYY-MM-DD')).then((result) => {
-        transactions = transactions.concat(result);
+        transactions = transactions.concat(result.filter((purchase) => {
+          return !nonPurchaseCategories.includes(purchase.category[0])
+        }));
       }).catch((err) => {
         console.log(err);
       }));
     };
-    Promise.all(promises).then(()=> {return transactions}).then((transactions) => {
-      transactions = transactions.filter((transaction) => {
-        return !['Bank Fees', 'Cash Advance' , 'Interest',  'Payment', 'Tax' ,'Transfer'].includes(transaction.category[0])
-      }).map((transaction) => [transaction.category[0], transaction.amount])
-        .reduce((acc, val) => {
-          val[0] in  acc ? acc[val[0]] += val[1] : acc[val[0]] = val[1];
-          return acc;
-        }, {});
-      return res.send(Object.entries(transactions).map(([key, value])=> ({name: key, value})));
-    });
+    return Promise.all(promises).then(() => { return callback(transactions)});
   });
-});
+}
+
+function getCategoryProportions(purchases){
+  purchases = purchases.map((purchase) => [purchase.category[0], purchase.amount])
+    .reduce((acc, val) => {
+      val[0] in  acc ? acc[val[0]] += val[1] : acc[val[0]] = val[1];
+      return acc;
+    }, {});
+  return Object.entries(purchases).map(([key, value])=> ({name: key, value}));
+}
+
