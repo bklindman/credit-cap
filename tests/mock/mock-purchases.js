70c8c96590460dffc3994e4d5e178e2a4956422a
bklindman <blake.lindman@gmail.com>
2019-06-11 15:33:15 -0700

    add category properties to sample purchases

--- tests/mock/mock-purchases.js
+++ tests/mock/mock-purchases.js
@@ -1,16 +1,19 @@
 const MerchantPurchases = [
   {
     category_id: "22001000",
+    category: ['Travel'],
     name: "Test Airlines",
     amount: 30
   },
   {
     category_id: "22001000",
+    category: ['Travel'],
     name: "Test Airlines",
     amount: 40
   },
   {
     category_id: "22001000",
+    category: ['Travel'],
     name: "Test Airlines",
     amount: 50
   }
@@ -19,24 +22,26 @@ const MerchantPurchases = [
 const DiningPurchases = [
   {
     category_id: "13005009",
+    category: ['Food & Dining'],
     name: "Restaurant",
     amount: 20
   },
   
   {
     category_id: "13005016",
+    category: ['Food & Dining'],
     name: "Restaurant",
     amount: 30
   },
   {
     category_id: "13005016",
+    category: ['Food & Dining'],
     name: "Restaurant",
     amount: 40
   }
 ]
 
 const MixedPurchases = MerchantPurchases.concat(DiningPurchases);
-console.log(MixedPurchases);
 
 module.exports = {
   MerchantPurchases,
