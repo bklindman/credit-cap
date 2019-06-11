f650131102fa889d80b80f8b57d6013fc21997b7
bklindman <blake.lindman@gmail.com>
2019-06-11 15:33:34 -0700

    create mock items

---
+++ tests/mock/mock-items.js
@@ -0,0 +1,18 @@
+const Items = [
+  {
+    institution_name: 'Test Bank',
+    item_id: '1204192cpj',
+    access_token: 'ajkfknsjefne',
+    accounts: ['Checking', 'Savings']
+  },
+  {
+    institution_name: 'Test Express',
+    item_id: 'qwnrfqkjaedn',
+    access_token: 'qonfkjsed',
+    accounts: ['Checking', 'Credit']
+  }
+];
+
+module.exports = {
+  Items
+};
